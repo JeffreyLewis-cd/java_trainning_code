32c3034a1319cf5e32b22789adf3ae88dc35f4f6
jiangfan6 <jiangfan6@outlook.com>
2018-05-09 22:09:00 +0800

    set

--- studentAndCourse/src/TestGeneric.java
+++ studentAndCourse/src/TestGeneric.java
@@ -12,7 +12,7 @@ public class TestGeneric {
     public void testAdd(){
         Course cr1=new Course("1","大学语文");
         courses.add(cr1);
-        /*泛型集合中，不能添加规定以外的类型！*/
+        /*泛型集合中，不能添加泛型规定的类型及其子类型以外的对象！*/
 //        courses.add("能否添加奇怪的东西？");
 
         Course cr2=new Course("2","Java基础");
@@ -26,10 +26,27 @@ public class TestGeneric {
         }
     }
 
+    /*泛型集合可以添加泛型的子类型的对象实例*/
+    public void testChild(){
+        ChildCourse ccr=new ChildCourse();
+        ccr.id="3";
+        ccr.name="我是子类型的课程对象实例~~";
+        courses.add(ccr);
+    }
+
+    /*泛型不能使用基本类型*/
+    public void testBasicType(){
+        List<Integer>list=new ArrayList<Integer>();
+        list.add(1);
+        System.out.println("基本类型必须使用包装类作为泛型: "+list.get(0));
+    }
+
     public static void main(String[] args) {
         TestGeneric tg = new TestGeneric();
         tg.testAdd();
         tg.testForEach();
-
+        tg.testChild();
+        tg.testForEach();
+        tg.testBasicType();
     }
 }
